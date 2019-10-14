b88b3083b7d76f3f8e7753fc49cb48c553e78200
AzatErnis <azat.ernis@gmail.com>
2019-10-14 23:39:50 +0600

    Updates

---
+++ precache-manifest.034d178d03c91488d0913724a1f46b6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "724ebbaa132d0df40a66bb3ed7a615dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "adeeea42b295eeffd7cc",
+    "url": "/monsters-rolodex/static/css/main.9d472290.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "adeeea42b295eeffd7cc",
+    "url": "/monsters-rolodex/static/js/main.bb5f2aac.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
